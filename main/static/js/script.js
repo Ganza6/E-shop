562b5cdb0fcab6ab1fd872f9dc062fc75b9c9cb5
Denis Koshelev <koden1998@mail.ru>
2018-10-11 02:19:27 +0300

    Внедряю jquery потихоньку

---
+++ main/static/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var form = $('#form');
+    form.on('submit',function(e){
+    e.preventDefault();
+    var nmb = $('#number').val();
+    var submit_btn = $('#submit_btn');
+    console.log(submit_btn);
+    var product_id = submit_btn.data('product_id');
+    var product_name = submit_btn.data("product_name");
+    var product_price = submit_btn.data("product_price");
+    console.log(product_id);
+    console.log(product_name);
+    console.log(nmb);
+    console.log(product_price);
+    })
+//$("#hidden").css("display", "none;"); // Для скрытия
+//$("#hidden").css("display", "block;"); // Для показа
+$('#kor').hover(function(){
+  $(".basket-items").slideToggle();
+});
+
+    });
